18e98c1d58fc9532a0af51a80ad58a7386318a9a
sylee3000 <dltmddud603@gmail.com>
2022-06-23 15:27:49 +0900

    220623-1

---
+++ S2/BOJ15666_N과 M (12).py
@@ -0,0 +1,21 @@
+N, M = map(int, input().split())
+lst = sorted(list(map(int, input().split())))
+seq = []
+seq_index = []
+seq_print = []
+def backtracking9(a):
+    if len(seq) == M:
+        seq_print.append(tuple(seq))
+        return
+    else:
+        for i in range(a, N):
+            if not seq or lst[i] >= seq[-1]:
+                seq.append(lst[i])
+                seq_index.append(i)
+                backtracking9(i)
+                seq.pop()
+                seq_index.pop()
+backtracking9(0)
+seq_print = sorted(set(seq_print))
+for i in seq_print:
+    print(*i)
